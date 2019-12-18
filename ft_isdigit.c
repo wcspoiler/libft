a90cf9eb8b3c05dfb56fefc4d601821f0c8bf4e3
Ripcord Eleanora <releanor@oa-g6.21-school.ru>
2019-12-18 13:07:26 +0300

    optimized ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,14 +6,11 @@
 /*   By: releanor <releanor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 04:16:53 by releanor          #+#    #+#             */
-/*   Updated: 2019/12/18 12:15:51 by releanor         ###   ########.fr       */
+/*   Updated: 2019/12/18 13:06:44 by releanor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return (c >= '0' && c <= '9') ? 1 : 0;
 }
